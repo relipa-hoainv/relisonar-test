794650e16e15cc58965861d0db5f15e8a55bf1d0
Nguyen Van Hoai <hoainv@relipasoft.com>
2018-09-24 15:58:50 +0700

    change file

--- app/User.php
+++ app/User.php
@@ -28,6 +28,6 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    protected $test = 'test';
+    protected $test = 'testtest';
     protected $test2 = 'test2';
 }
